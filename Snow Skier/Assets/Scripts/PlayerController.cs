3abdf5bf643b76586ed94ca306a5152ca346b546
Gökhan güneş <132ggokhann@gmail.com>
2023-03-07 19:00:34 +0300

    torque added
    version 1.5

---
+++ Snow Skier/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+   
+    [SerializeField] private float torq = 1f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddTorque(torq);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddTorque(-torq);
+        }
+    }
+}
