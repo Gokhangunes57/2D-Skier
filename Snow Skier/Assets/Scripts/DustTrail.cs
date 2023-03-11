e9e7fe42d0575f520f87cd5c7bd3490a7d2dec39
Gökhan güneş <132ggokhann@gmail.com>
2023-03-11 18:38:50 +0300

    Dust trail completed
    version 2.3

--- Snow Skier/Assets/Scripts/DustTrail.cs
+++ Snow Skier/Assets/Scripts/DustTrail.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DustTrail : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private ParticleSystem dustTrail;
+
+
+    private void OnCollisionExit2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            dustTrail.Stop();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D col)
     {
-        
+        if (col.gameObject.CompareTag("Ground"))
+        {
+            dustTrail.Play();
+        }
     }
 }
