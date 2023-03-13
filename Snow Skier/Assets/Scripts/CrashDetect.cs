724f19122fae2e79b958cce310d523324988575a
Gökhan güneş <132ggokhann@gmail.com>
2023-03-13 19:46:17 +0300

    updated

--- Snow Skier/Assets/Scripts/CrashDetect.cs
+++ Snow Skier/Assets/Scripts/CrashDetect.cs
@@ -9,13 +9,14 @@ public class CrashDetect : MonoBehaviour
     [SerializeField] private float delay = 0.5f;
     [SerializeField] ParticleSystem crashParticles;
     [SerializeField] AudioClip crashSound;
-
+    bool HasCrashed = false;
     
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.CompareTag("Ground"))
+        if (col.CompareTag("Ground")&& !HasCrashed)
         {
+            HasCrashed = true;
             FindObjectOfType<PlayerController>().DisableControls();
             crashParticles.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSound);
